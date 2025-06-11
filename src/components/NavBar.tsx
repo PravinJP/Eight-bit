441fcef37997e6a10408f4245daaf4eb4d28ea80
Sabarish V S <Sabarish_7@protonmail.com>
2025-06-11 19:39:14 +0530

    added noise and gradient

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,22 @@
 const NavBar = () => {
   return (
-    <div className="w-full max-w-[1390px] px-4 py-3.5 bg-white/10 backdrop-blur-[300px] rounded-[72px] flex justify-between items-center">
-      <div className="logo flex flex-row align-left items-center gap-2 mr-auto">
-         <img scale-90 origin-left src="/logo.png" alt="" />
-      <p className="text-2xl font-semibold text-white items-center">Eight Bit Tech</p>
-      </div>
-     
-      <div className="navigation-links flex flex-row items-center gap-8 mx-auto">
-        <h3 className="text-white font-medium text-xl">About</h3>
-        <h3 className="text-white font-medium text-xl">Help & Support</h3>
-        <h3 className="text-white font-medium text-xl">Services</h3>
-        <h3 className="text-white font-medium text-xl">Products</h3>
-      </div>
-      <div className="action-button-container flex ml-auto">
-        <button className="text-black font-semibold flex  text-lg bg-white px-4 py-3 rounded-3xl transition-colors">
-          Contact us
+    <div className="gradient-border flex  max-w-[1390px] w-full p-[3px] bg-gradient-to-tr from-white/15  to-black/0  rounded-[72px]">
+      <div className="w-full max-w-[1390px] px-4 py-3.5 bg-[#FEFEFE]/10 backdrop-blur-[150px] rounded-[72px] flex justify-between items-center">
+        {/* logo */}
+        <img scale-90 origin-left src="/logo-font.svg" alt="" />
+
+        <div className="navigation-links flex flex-row items-center gap-8 mx-auto text-white font-medium text-xl">
+          <h3>About</h3>
+          <h3>Help & Support</h3>
+          <h3>Services</h3>
+          <h3>Products</h3>
+        </div>
+        <div className="action-button-container flex ml-auto">
+          <button className="text-black font-medium flex  text-lg bg-white px-4 py-3 rounded-3xl transition-colors">
+            Contact us
           </button>
-          </div>
+        </div>
+      </div>
     </div>
   );
 };
